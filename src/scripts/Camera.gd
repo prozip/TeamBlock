6b1e78ee7225af0b129f7db07ef23aca5d4319be
prozip <quachkimson2000@gmail.com>
2021-11-25 20:04:59 +0700

    up apk

--- src/scripts/Camera.gd
+++ src/scripts/Camera.gd
@@ -2,20 +2,35 @@ extends Camera
 
 var position = Vector2()
 var drag_speed = 0.02
+var drag_percent = 0.7
 var zoom_speed = 0.5
 
 export(int,"disabled","single_drag","multi_drag") var movement_gesture = 2
 export(int,"disabled","pinch") var zoom_gesture = 1
 
+onready var target: Object = get_parent().get_node("MainBlock")
+export var smooth_speed: float
+export var offset: Vector3
+onready var current_target_trans = Vector3(0,0.5,0)
+onready var default_fov = fov
+
+func _physics_process(delta: float) -> void:
+	print(fov, self.translation)
+	if(target != null):
+		current_target_trans = target.translation - current_target_trans
+		self.translation = lerp(self.translation, current_target_trans + offset, smooth_speed * delta)
+
 func set_position(p):
+	offset.x = p.y
+	offset.z = -p.x
 	transform.origin.x = p.y
 	transform.origin.z = -p.x
-
+	
 func _unhandled_input(event):
 	if (event is InputEventMultiScreenDrag and  movement_gesture == 2):
 		position.y = transform.origin.x
 		position.x = -transform.origin.z
-		set_position(position - event.relative * drag_speed * fov / 40)
+		set_position(position - event.relative * drag_speed * fov / default_fov)
 	elif event is InputEventScreenPinch and zoom_gesture == 1:
 		zoom(event)
 
